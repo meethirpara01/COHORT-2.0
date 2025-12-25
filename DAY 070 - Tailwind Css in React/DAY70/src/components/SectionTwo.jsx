8cd00bbd6425d9a50f2de5b4cdd00f9b9b52baf2
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-12-25 22:39:26 +0530

    Daily backup - $(25-dec-2025)

---
+++ DAY 070 - Tailwind Css in React/DAY70/src/components/SectionTwo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SectionTwo = () => {
+  return (
+    <div className='w-full '>
+        <div className="card1">
+            
+        </div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+        <div className="card1"></div>
+    </div>
+  )
+}
+
+export default SectionTwo
