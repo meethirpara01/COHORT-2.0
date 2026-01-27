ba977f67f531e712ba983df31ba6f67655fd54bb
Meet. Hirpara <meetkumarhirpara@gmail.com>
2026-01-27 16:19:09 +0530

    Daily backup - $(27-Jan-2025)

---
+++ BACKEND_2.0/DAY 086 - Programmimg Server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello User, Server is working!");
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
