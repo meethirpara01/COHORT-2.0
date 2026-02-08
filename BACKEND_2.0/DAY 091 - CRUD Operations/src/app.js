25a6fc516276e33e226c38a5b256ba913f423b59
Meet. Hirpara <meetkumarhirpara@gmail.com>
2026-02-09 01:08:55 +0530

    notes with database

---
+++ BACKEND_2.0/DAY 091 - CRUD Operations/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const noteModel = require("./models/notes.model")
+
+const app = express();
+app.use(express.json());
+
+
+app.post("/notes", async (req, res) => {
+    const { title, description } = req.body;
+
+    const note = await noteModel.create({
+        // title: title,
+        // description: description
+        title, description
+    });
+
+    res.status(201).json({
+        message: "Note created successfully",
+        note
+    })
+})
+
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find(); // IT WILL RETURN AN DATA IN FORM OF ARRAY
+
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
+
+module.exports = app;
