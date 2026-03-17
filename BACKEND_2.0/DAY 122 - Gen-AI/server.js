4218d9a748610a1be3a51e3f529d1a52c0762cdb
Meet. Hirpara <meetkumarhirpara@gmail.com>
2026-03-17 22:29:48 +0530

    AI integration

---
+++ BACKEND_2.0/DAY 122 - Gen-AI/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import app from './src/app.js';
+import connectToDB from './src/config/database.js';
+
+import { testAi } from './src/services/ai.service.js';
+
+connectToDB();
+
+testAi();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
